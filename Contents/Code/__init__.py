0b980777efec8279e00bf0801fda185dac7257c5
scyber <matt.p.walsh@gmail.com>
2012-04-06 22:57:30 -0400

    rewrite of processing logic.  Remove images I'm not sure I want to use.

--- Contents/Code/__init__.py
+++ Contents/Code/__init__.py
@@ -54,7 +54,7 @@ def Start():
   
   tvd = Resource.ExternalPath("tivodecode")
   if Platform.OS == 'Windows':
-		tvd = Resource.ExternalPath("tivodecode.exe")
+        tvd = Resource.ExternalPath("tivodecode.exe")
   Log.Debug(tvd)
   #os.chmod(tvd, 0755)
   Thread.Create(findTivos)
@@ -69,13 +69,14 @@ def getTivoNames():
     if myMAK == None:
       myMAK = ""
     if (len(myMAK) == 10):
-		tivo_list = Dict['tivos']
-		Log.Debug(tivo_list)
-		for tivo in tivo_list:
-			dir.Append(Function(DirectoryItem(getTivoShows, tivo), tivoName = tivo))
+        tivo_list = Dict['tivos']
+        Log.Debug(tivo_list)
+        for tivo in tivo_list:
+            dir.Append(Function(DirectoryItem(getTivoShows, tivo), tivoName = tivo))
 
 
     dir.Append(PrefsItem("Preferences"))
+    dir.Append(Function(DirectoryItem(findTivosFromMenu, "Find Tivos")))
     return dir
 
 ####################################################################################################
@@ -142,7 +143,7 @@ def getTivoShowsByIPURL(tivoip, url, dir, expand_name):
       show_total_items = int(getNameFromXML(show, "g:Details/g:TotalItems/text()"))
       show_folder_url = getNameFromXML(show, "g:Links/g:Content/g:Url/text()")
       show_folder_id = show_folder_url[show_folder_url.rfind("%2F")+3:]
-      item = Function(DirectoryItem(getTivoEpisodes, title="[" + show_name + "]", thumb=R("art-folder.png")), tivoip=tivoip, show_id=show_folder_id, showname=show_name)
+      item = Function(DirectoryItem(getTivoEpisodes, title="[" + show_name + "]", thumb=R("art-default.jpg")), tivoip=tivoip, show_id=show_folder_id, showname=show_name)
       dir.Append(item)
 
     elif ((show_content_type == TIVO_CONTENT_SHOW_TTS) or
@@ -173,9 +174,9 @@ def getTivoShowsByIPURL(tivoip, url, dir, expand_name):
         target_name = extra_name
       if show_copyright != "Yes" and show_in_progress != "Yes":
         
-        url = "http://"+Network.Address+":" + str(TIVO_PORT) + "/" + tivoip + "/" + base64.b64encode(show_url, "_;")	
+        url = "http://"+Network.Address+":" + str(TIVO_PORT) + "/" + tivoip + "/" + base64.b64encode(show_url, "_;")    
         #itempath = TIVO_PLUGIN_PREFIX + "/" +TIVO_GET_SHOW +"/" + tivoip +"/" + base64.b64encode(show_url, "_;") + "/" + base64.b64encode(show_name+" : "+extra_name, "_;")
-        item = VideoItem(url,  title=target_name, summary=show_desc, duration=show_duration,thumb=R("art-video.png")) 
+        item = VideoItem(url,  title=target_name, summary=show_desc, duration=show_duration,thumb=R("art-default.jpg")) 
         if (show_episode_num != ""):
           subtitle = "Season " + show_season_num + "      Episode " + show_season_ep_num
           item.subtitle = subtitle
@@ -218,27 +219,31 @@ class MyVideoHandler(BaseHTTPRequestHandler):
       self.send_header('Content-type', 'video/mpeg2')
       self.send_header('Accept-Ranges', 'none')
       self.end_headers()
-      Log.Debug(self.headers)
-
       
-
-      authhandler = urllib2.HTTPDigestAuthHandler()
-      authhandler.add_password("TiVo DVR", url, "tivo", Prefs["MAK"])
-      Log.Debug("Starting httpstuff1")
-      opener= urllib2.build_opener(authhandler, urllib2.HTTPCookieProcessor(CookiesJar))
-      Log.Debug("Starting httpstuff2 : " + url)
-      req = opener.open(url)
-
+      q = Queue()
+      
+      #This is the subprocess to decode the tivo stream
       tivodecode = Helper.Process("tivodecode", "-m", Prefs["MAK"], "-")
-      run_flag =[]
+      #This thread fetches from the tivo and writes it to the queue.
+      tf = Thread.Create(tivoFetcher, True,url, q, tivodecode)
       
-     #Thread.Create(tivoFetcher,False, req, tivodecode, self)
-      thread.start_new_thread(tivoFetcher,(req, tivodecode, self))
+      #This thread reads the data from the queue, then sends it to tivodecode
+      td = Thread.Create(tivoDecoder, True, q, tivodecode)
+      
+      CHUNK = 4192
+      status = 0
+      starttime = time()
+      
+      #Read the data from tivodecode and output it to the client
       while True:
-          data = tivodecode.stdout.read(4 * 1024)
-          #Log.Debug("Decoding...")
-          if not data:
-                Log.Debug("Quitting tivoDecoder")              
+          data = tivodecode.stdout.read(CHUNK)
+          #if (CHUNK < (16 * 1024 * 1024)):
+          #  CHUNK = CHUNK * 2
+          #status += CHUNK
+          #currenttime = time()
+          #if ((currenttime-starttime) !=0):
+          #  Log.Debug(((status/(1024*1024))*8)/(currenttime-starttime)) 
+          if not data:             
                 break
           self.wfile.write(data)
  
@@ -258,7 +263,7 @@ class MyVideoHandler(BaseHTTPRequestHandler):
          Log.Debug("Attmpling to kill tvd:")
          Log.Debug(tivodecode.pid)
          if not winKill(tivodecode.pid): 
-		  Log.Debug("Couldn't kill")
+          Log.Debug("Couldn't kill")
       else:
         kill(tivodecode.pid, SIGTERM)
     except:
@@ -273,28 +278,57 @@ class MyVideoHandler(BaseHTTPRequestHandler):
     Log.Debug("Got an error..")
     
 
-###########################################################3		
+###########################################################3        
             
-def tivoFetcher (req, tivodecode, httphandler ):
-      
-      CHUNK = 4 * 1024
-      while True:
-        #Log.Debug("reading from tivo....")
-        chunk = req.read(CHUNK)
+def tivoFetcher (url, q, tvd ):
+      authhandler = urllib2.HTTPDigestAuthHandler()
+      authhandler.add_password("TiVo DVR", url, "tivo", Prefs["MAK"])
+      Log.Debug("Starting httpstuff1")
+      opener= urllib2.build_opener(authhandler, urllib2.HTTPCookieProcessor(CookiesJar))
+      Log.Debug("Starting httpstuff2 : " + url)      
+      req = opener.open(url)
+      CHUNK = 1 * 1024 * 1024
+      status = 0
+      starttime = time()
+      while True and tvd.poll() !=0:
+        Log.Debug("reading from tivo....")
+        #Code to increment chunk size.  In theory, give the client some data to start dispalying asap, and then ramp up speeds to keep up with stream.
+        #if (CHUNK < (4 * 1024 * 1024)):
+        #    CHUNK = CHUNK * 2
+        data = req.read(CHUNK)
+        #Code to report mbps for each loop (assuming I got the math right)
+        #status += CHUNK
+        #currenttime = time()
+        #if ((currenttime-starttime) !=0):
+        #    Log.Debug(((status/(1024*1024))*8)/(currenttime-starttime))        
         #Log.Debug("read from tivo....")
-        if not chunk: break
+        if not data: break
         #Log.Debug("writing to tvd...")
         try:
-          tivodecode.stdin.write(chunk)
+          q.put(data)
         except:
-			Log.Debug("tvd closed...")
-			break
+          Log.Debug("cannot add to queue")
+          break
+        
       Log.Debug("done fetching")
-      try:
-        tivodecode.stdin.close()
-      except:
-		  Log.Debug("tvd already closed")
-			
+      
+        
+#################################################################
+
+def tivoDecoder (q, tvd):
+    while True and tvd.poll() != 0:
+        data = q.get()
+        try:
+            tvd.stdin.write(data)
+        except:
+            Log.Debug("tvd already closed")
+    try:
+        tvd.stdin.close()
+    except:
+        Log.Debug("tvd already closed")
+    
+
+            
 #################################################################        
 def winKill(pid):
     """kill function for Win32"""
@@ -314,97 +348,104 @@ def TivoServerThread(ip, port):
   
 ####################################################################################################
 
+def findTivosFromMenu(sender):
+    Thread.Create(findTivos)
+    mc = MessageContainer("Finding Tivos","Please wait while the Tivos are discovered.   You may need to exit the tivo plugin to display any newly discovered tivos.")
+    return mc
+
+
 
 def findTivos():
-	regtype  = '_tivo-videos._tcp'
-	timeout  = 7
-	queried  = []
-	resolved = []
-	Dict['tivos'] = {}
-	current_tivo = {}
+    regtype  = '_tivo-videos._tcp'
+    timeout  = 7
+    queried  = []
+    resolved = []
+    Dict['tivos'] = {}
+    current_tivo = {}
 
-	def query_record_callback(sdRef, flags, interfaceIndex, errorCode, fullname,
-							  rrtype, rrclass, rdata, ttl):
-		if errorCode == pybonjour.kDNSServiceErr_NoError:
-			tivo_list = Dict['tivos']
-			for tivo in tivo_list:
-				if tivo_list[tivo]['hosttarget'] == fullname:
-					Log.Debug("Queried: "+tivo)
-					tivo_list[tivo]['ip'] = socket.inet_ntoa(rdata)
-			queried.append(True)
-
-
-	def resolve_callback(sdRef, flags, interfaceIndex, errorCode, fullname, hosttarget, port, txtRecord):
-		if errorCode != pybonjour.kDNSServiceErr_NoError:
-			return
-		shortName = fullname.replace('._tivo-videos._tcp.local.','').replace("\\032"," ")
-		Log.Debug("Resolved: "+shortName)
-		Dict['tivos'][shortName] = {'interfaceIndex': interfaceIndex,'port': port,'fullname': fullname,'hosttarget': hosttarget}
-		query_sdRef = pybonjour.DNSServiceQueryRecord(interfaceIndex = interfaceIndex,
-											fullname = hosttarget,
-											rrtype = pybonjour.kDNSServiceType_A,
-											callBack = query_record_callback)
-
-		try:
-			while not queried:
-				ready = select.select([query_sdRef], [], [], timeout)
-				if query_sdRef not in ready[0]:					
-					break
-				pybonjour.DNSServiceProcessResult(query_sdRef)
-			else:
-				queried.pop()
-		finally:
-			query_sdRef.close()
-
-		resolved.append(True)
+    def query_record_callback(sdRef, flags, interfaceIndex, errorCode, fullname,
+                              rrtype, rrclass, rdata, ttl):
+        if errorCode == pybonjour.kDNSServiceErr_NoError:
+            tivo_list = Dict['tivos']
+            for tivo in tivo_list:
+                if tivo_list[tivo]['hosttarget'] == fullname:
+                    Log.Debug("Queried: "+tivo)
+                    tivo_list[tivo]['ip'] = socket.inet_ntoa(rdata)
+            queried.append(True)
 
 
-	def browse_callback(sdRef, flags, interfaceIndex, errorCode, serviceName,
-						regtype, replyDomain):
-		if errorCode != pybonjour.kDNSServiceErr_NoError:
-			return
+    def resolve_callback(sdRef, flags, interfaceIndex, errorCode, fullname, hosttarget, port, txtRecord):
+        if errorCode != pybonjour.kDNSServiceErr_NoError:
+            return
+        shortName = fullname.replace('._tivo-videos._tcp.local.','').replace("\\032"," ")
+        Log.Debug("Resolved: "+shortName)
+        Dict['tivos'][shortName] = {'interfaceIndex': interfaceIndex,'port': port,'fullname': fullname,'hosttarget': hosttarget}
+        query_sdRef = pybonjour.DNSServiceQueryRecord(interfaceIndex = interfaceIndex,
+                                            fullname = hosttarget,
+                                            rrtype = pybonjour.kDNSServiceType_A,
+                                            callBack = query_record_callback)
 
-		if not (flags & pybonjour.kDNSServiceFlagsAdd):
-			return
-		Log.Debug("Browsed: "+serviceName)
-		Dict['tivos'][serviceName] = {}
-		
-	
-		resolve_sdRef = pybonjour.DNSServiceResolve(0,
-													interfaceIndex,
-													serviceName,
-													regtype,
-													replyDomain,
-													resolve_callback)
+        try:
+            while not queried:
+                ready = select.select([query_sdRef], [], [], timeout)
+                if query_sdRef not in ready[0]:                    
+                    break
+                pybonjour.DNSServiceProcessResult(query_sdRef)
+            else:
+                queried.pop()
+        finally:
+            query_sdRef.close()
 
-		try:
-			while not resolved:
-				ready = select.select([resolve_sdRef], [], [], timeout)
-				if resolve_sdRef not in ready[0]:
-					Log.Debug('Resolve timed out')
-					break
-				pybonjour.DNSServiceProcessResult(resolve_sdRef)
-			else:
-				resolved.pop()
-		finally:
-			resolve_sdRef.close()
+        resolved.append(True)
 
 
-	browse_sdRef = pybonjour.DNSServiceBrowse(regtype = regtype,
-											  callBack = browse_callback)
-	
-	starttime = time()
-	try:
-		try:
-			while True and (time() - starttime) < 10:
-				ready = select.select([browse_sdRef], [], [], 1)
-				if browse_sdRef in ready[0]:
-					pybonjour.DNSServiceProcessResult(browse_sdRef)
-		except KeyboardInterrupt:
-			pass
-	finally:
-		browse_sdRef.close()	
-		
+    def browse_callback(sdRef, flags, interfaceIndex, errorCode, serviceName,
+                        regtype, replyDomain):
+        if errorCode != pybonjour.kDNSServiceErr_NoError:
+            return
+
+        if not (flags & pybonjour.kDNSServiceFlagsAdd):
+            return
+        Log.Debug("Browsed: "+serviceName)
+        Dict['tivos'][serviceName] = {}
+        
+    
+        resolve_sdRef = pybonjour.DNSServiceResolve(0,
+                                                    interfaceIndex,
+                                                    serviceName,
+                                                    regtype,
+                                                    replyDomain,
+                                                    resolve_callback)
+
+        try:
+            while not resolved:
+                ready = select.select([resolve_sdRef], [], [], timeout)
+                if resolve_sdRef not in ready[0]:
+                    Log.Debug('Resolve timed out')
+                    break
+                pybonjour.DNSServiceProcessResult(resolve_sdRef)
+            else:
+                resolved.pop()
+        finally:
+            resolve_sdRef.close()
+
+
+    browse_sdRef = pybonjour.DNSServiceBrowse(regtype = regtype,
+                                              callBack = browse_callback)
+    
+    starttime = time()
+    try:
+        try:
+            while True and (time() - starttime) < 10:
+                ready = select.select([browse_sdRef], [], [], 1)
+                if browse_sdRef in ready[0]:
+                    pybonjour.DNSServiceProcessResult(browse_sdRef)
+        except KeyboardInterrupt:
+            pass
+    finally:
+        browse_sdRef.close()
+      
+        
 
         
 
